6ec612ebf33ace83f40a9634afbfed7347f11021
Lisong Guo <lisong.guo@me.com>
2017-04-07 00:01:35 +0200

    warmup

--- machine-learning-ex1/ex1/plotData.m
+++ machine-learning-ex1/ex1/plotData.m
@@ -16,9 +16,10 @@ figure; % open a new figure window
 %       appear as red crosses. Furthermore, you can make the
 %       markers larger by using plot(..., 'rx', 'MarkerSize', 10);
 
+plot(x, y, 'rx', 'MarkerSize', 10)
 
-
-
+xlabel('population')
+ylabel('profit')
 
 % ============================================================
 
